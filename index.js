41b3c965997efc7c94d9921e948c7b20e4c2d5b2
Cassie Andrews <candrews1@ufl.edu>
2024-10-15 19:07:38 -0400

    updated form.onsubmit function

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 // Your code here
+var URL = "https://api.openweathermap.org/data/2.5/weather";
 var weatherAppDiv = document.getElementById('weather-app');
-var formSection = document.getElementById('weather');
-var inputField = document.getElementsById('weather-search');
-var inputFieldLabel = document.querySelector('label');
-var submitButton = document.querySelector('button');
+var form = document.querySelector('form');
 
+// https://api.openweathermap.org/data/2.5/weather?zip=34205&appid=a8b8566d914e7ee5f3e4973ebeb94b48
+
+// API Key: a8b8566d914e7ee5f3e4973ebeb94b48
 
 // When the app loads
     // only the input field and search button should be visible
     // already in the HTML
 
-
 // ON SUBMIT 
+form.onsubmit = function(e) {
+    e.preventDefault();
+    // console.log('click');
+    fetch(URL)
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
     // app should call the Open Weather API's current weather endpoint using the JS fetch API to obtain weather data based on location entered by the user
-
+    
     // input field's value is reset with each form submission
     // additional searches should not cause additional weather data to be added to the #weather element
     // each subsequent query should produce either a new display or a NOT FOUND message
